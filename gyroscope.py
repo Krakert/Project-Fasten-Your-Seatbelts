166bf3bfcce21998f55325a0ad050d36540f6ccf
Milo Broerse <milo.broerse@gmail.com>
2019-10-14 23:11:00 +0200

    Need to Refactor code

--- gyroscope.py
+++ gyroscope.py
@@ -1,214 +1,70 @@
+#!/usr/bin/python
 import smbus
-import time
-
-class mpu6050:
-
-    # Global Variables
-    GRAVITIY_MS2 = 9.80665
-    address = None
-    bus = None
-
-    # Scale Modifiers
-    ACCEL_SCALE_MODIFIER_2G = 16384.0
-    ACCEL_SCALE_MODIFIER_4G = 8192.0
-    ACCEL_SCALE_MODIFIER_8G = 4096.0
-    ACCEL_SCALE_MODIFIER_16G = 2048.0
-
-    GYRO_SCALE_MODIFIER_250DEG = 131.0
-    GYRO_SCALE_MODIFIER_500DEG = 65.5
-    GYRO_SCALE_MODIFIER_1000DEG = 32.8
-    GYRO_SCALE_MODIFIER_2000DEG = 16.4
-
-    # Pre-defined ranges
-    ACCEL_RANGE_2G = 0x00
-    ACCEL_RANGE_4G = 0x08
-    ACCEL_RANGE_8G = 0x10
-    ACCEL_RANGE_16G = 0x18
-
-    GYRO_RANGE_250DEG = 0x00
-    GYRO_RANGE_500DEG = 0x08
-    GYRO_RANGE_1000DEG = 0x10
-    GYRO_RANGE_2000DEG = 0x18
-
-    # MPU-6050 Registers
-    PWR_MGMT_1 = 0x6B
-    PWR_MGMT_2 = 0x6C
-
-    ACCEL_XOUT0 = 0x3B
-    ACCEL_YOUT0 = 0x3D
-    ACCEL_ZOUT0 = 0x3F
-
-    TEMP_OUT0 = 0x41
-
-    GYRO_XOUT0 = 0x43
-    GYRO_YOUT0 = 0x45
-    GYRO_ZOUT0 = 0x47
-
-    ACCEL_CONFIG = 0x1C
-    GYRO_CONFIG = 0x1B
-
-    def __init__(self, address, bus=1):
-        self.address = address
-        self.bus = smbus.SMBus(bus)
-        # Wake up the MPU-6050 since it starts in sleep mode
-        self.bus.write_byte_data(self.address, self.PWR_MGMT_1, 0x00)
-
-    # I2C communication methods
-
-    def read_i2c_word(self, register):
-        # Read the data from the registers
-        high = self.bus.read_byte_data(self.address, register)
-        low = self.bus.read_byte_data(self.address, register + 1)
-
-        value = (high << 8) + low
-
-        if (value >= 0x8000):
-            return -((65535 - value) + 1)
-        else:
-            return value
-
-    def set_accel_range(self, accel_range):
-        # First change it to 0x00 to make sure we write the correct value later
-        self.bus.write_byte_data(self.address, self.ACCEL_CONFIG, 0x00)
-
-        # Write the new range to the ACCEL_CONFIG register
-        self.bus.write_byte_data(self.address, self.ACCEL_CONFIG, accel_range)
-
-    def read_accel_range(self, raw = False):
-        raw_data = self.bus.read_byte_data(self.address, self.ACCEL_CONFIG)
-
-        if raw is True:
-            return raw_data
-        elif raw is False:
-            if raw_data == self.ACCEL_RANGE_2G:
-                return 2
-            elif raw_data == self.ACCEL_RANGE_4G:
-                return 4
-            elif raw_data == self.ACCEL_RANGE_8G:
-                return 8
-            elif raw_data == self.ACCEL_RANGE_16G:
-                return 16
-            else:
-                return -1
-
-    def get_accel_data(self, g = False):
-        x = self.read_i2c_word(self.ACCEL_XOUT0)
-        y = self.read_i2c_word(self.ACCEL_YOUT0)
-        z = self.read_i2c_word(self.ACCEL_ZOUT0)
-
-        accel_scale_modifier = None
-        accel_range = self.read_accel_range(True)
-
-        if accel_range == self.ACCEL_RANGE_2G:
-            accel_scale_modifier = self.ACCEL_SCALE_MODIFIER_2G
-        elif accel_range == self.ACCEL_RANGE_4G:
-            accel_scale_modifier = self.ACCEL_SCALE_MODIFIER_4G
-        elif accel_range == self.ACCEL_RANGE_8G:
-            accel_scale_modifier = self.ACCEL_SCALE_MODIFIER_8G
-        elif accel_range == self.ACCEL_RANGE_16G:
-            accel_scale_modifier = self.ACCEL_SCALE_MODIFIER_16G
-        else:
-            print("Unknown range-accel_scale_modifier set to self.ACCEL_SCALE_MODIFIER_2G")
-            accel_scale_modifier = self.ACCEL_SCALE_MODIFIER_2G
-
-        x = x / accel_scale_modifier
-        y = y / accel_scale_modifier
-        z = z / accel_scale_modifier
-
-        if g is True:
-            return {'x': x, 'y': y, 'z': z}
-        elif g is False:
-            x = x * self.GRAVITIY_MS2
-            y = y * self.GRAVITIY_MS2
-            z = z * self.GRAVITIY_MS2
-            return {'x': x, 'y': y, 'z': z}
-
-    def set_gyro_range(self, gyro_range):
-        # First change it to 0x00 to make sure we write the correct value later
-        self.bus.write_byte_data(self.address, self.GYRO_CONFIG, 0x00)
-
-        # Write the new range to the ACCEL_CONFIG register
-        self.bus.write_byte_data(self.address, self.GYRO_CONFIG, gyro_range)
-
-    def read_gyro_range(self, raw = False):
-        raw_data = self.bus.read_byte_data(self.address, self.GYRO_CONFIG)
-
-        if raw is True:
-            return raw_data
-        elif raw is False:
-            if raw_data == self.GYRO_RANGE_250DEG:
-                return 250
-            elif raw_data == self.GYRO_RANGE_500DEG:
-                return 500
-            elif raw_data == self.GYRO_RANGE_1000DEG:
-                return 1000
-            elif raw_data == self.GYRO_RANGE_2000DEG:
-                return 2000
-
-            else:
-                return -1
-
-    def get_gyro_data(self):
-        x = self.read_i2c_word(self.GYRO_XOUT0)
-        y = self.read_i2c_word(self.GYRO_YOUT0)
-        z = self.read_i2c_word(self.GYRO_ZOUT0)
-
-        gyro_scale_modifier = None
-        gyro_range = self.read_gyro_range(True)
-
-        if gyro_range == self.GYRO_RANGE_250DEG:
-            gyro_scale_modifier = self.GYRO_SCALE_MODIFIER_250DEG
-        elif gyro_range == self.GYRO_RANGE_500DEG:
-            gyro_scale_modifier = self.GYRO_SCALE_MODIFIER_500DEG
-        elif gyro_range == self.GYRO_RANGE_1000DEG:
-            gyro_scale_modifier = self.GYRO_SCALE_MODIFIER_1000DEG
-        elif gyro_range == self.GYRO_RANGE_2000DEG:
-            gyro_scale_modifier = self.GYRO_SCALE_MODIFIER_2000DEG
-        else:
-            gyro_scale_modifier = self.GYRO_SCALE_MODIFIER_250DEG
-
-        x = x / gyro_scale_modifier
-        y = y / gyro_scale_modifier
-        z = self.read_i2c_word(self.GYRO_ZOUT0)
-
-        gyro_scale_modifier = None
-        gyro_range = self.read_gyro_range(True)
-
-        if gyro_range == self.GYRO_RANGE_250DEG:
-            gyro_scale_modifier = self.GYRO_SCALE_MODIFIER_250DEG
-        elif gyro_range == self.GYRO_RANGE_500DEG:
-            gyro_scale_modifier = self.GYRO_SCALE_MODIFIER_500DEG
-        elif gyro_range == self.GYRO_RANGE_1000DEG:
-            gyro_scale_modifier = self.GYRO_SCALE_MODIFIER_1000DEG
-        elif gyro_range == self.GYRO_RANGE_2000DEG:
-            gyro_scale_modifier = self.GYRO_SCALE_MODIFIER_2000DEG
-        else:
-            gyro_scale_modifier = self.GYRO_SCALE_MODIFIER_250DEG
-
-        x = x / gyro_scale_modifier
-        y = y / gyro_scale_modifier
-        z = z / gyro_scale_modifier
-
-        return {'x': x, 'y': y, 'z': z}
-
-    def get_all_data(self):
-        temp = self.get_temp()
-        accel = self.get_accel_data()
-        gyro = self.get_gyro_data()
-
-        return [accel, gyro, temp]
-
-mpu = mpu6050(0x68)
-
-if __name__ == "__main__":
-    while (1):
-        try:
-           accel_data = mpu.get_accel_data()
-           gyro_data = mpu.get_gyro_data()
-
-           print("Ax:{:.4f}\tAy:{:.4f}\tAz:{:.4f}\tGx:{:.4f}\tGy:{:.4f}\tGz:{:.4f} ".format(accel_data['x'], accel_data['y'], accel_data['z'], gyro_data['x'], gyro_data['y'], gyro_data['z']))
-
-        except KeyboardInterrupt:
-            break
-
-        time.sleep(0.5)
+import math
+ 
+# Register
+power_mgmt_1 = 0x6b
+power_mgmt_2 = 0x6c
+ 
+def read_byte(reg):
+    return bus.read_byte_data(address, reg)
+ 
+def read_word(reg):
+    h = bus.read_byte_data(address, reg)
+    l = bus.read_byte_data(address, reg+1)
+    value = (h << 8) + l
+    return value
+ 
+def read_word_2c(reg):
+    val = read_word(reg)
+    if (val >= 0x8000):
+        return -((65535 - val) + 1)
+    else:
+        return val
+ 
+def dist(a,b):
+    return math.sqrt((a*a)+(b*b))
+ 
+def get_y_rotation(x,y,z):
+    radians = math.atan2(x, dist(y,z))
+    return -math.degrees(radians)
+ 
+def get_x_rotation(x,y,z):
+    radians = math.atan2(y, dist(x,z))
+    return math.degrees(radians)
+ 
+bus = smbus.SMBus(1) # bus = smbus.SMBus(0) fuer Revision 1
+address = 0x68       # via i2cdetect
+ 
+# Aktivieren, um das Modul ansprechen zu koennen
+bus.write_byte_data(address, power_mgmt_1, 0)
+ 
+print "Gyroskop"
+print "--------"
+ 
+gyroskop_xout = read_word_2c(0x43)
+gyroskop_yout = read_word_2c(0x45)
+gyroskop_zout = read_word_2c(0x47)
+ 
+print "gyroskop_xout: ", ("%5d" % gyroskop_xout), " skaliert: ", (gyroskop_xout / 131)
+print "gyroskop_yout: ", ("%5d" % gyroskop_yout), " skaliert: ", (gyroskop_yout / 131)
+print "gyroskop_zout: ", ("%5d" % gyroskop_zout), " skaliert: ", (gyroskop_zout / 131)
+ 
+print
+print "Beschleunigungssensor"
+print "---------------------"
+ 
+beschleunigung_xout = read_word_2c(0x3b)
+beschleunigung_yout = read_word_2c(0x3d)
+beschleunigung_zout = read_word_2c(0x3f)
+ 
+beschleunigung_xout_skaliert = beschleunigung_xout / 16384.0
+beschleunigung_yout_skaliert = beschleunigung_yout / 16384.0
+beschleunigung_zout_skaliert = beschleunigung_zout / 16384.0
+ 
+print "beschleunigung_xout: ", ("%6d" % beschleunigung_xout), " skaliert: ", beschleunigung_xout_skaliert
+print "beschleunigung_yout: ", ("%6d" % beschleunigung_yout), " skaliert: ", beschleunigung_yout_skaliert
+print "beschleunigung_zout: ", ("%6d" % beschleunigung_zout), " skaliert: ", beschleunigung_zout_skaliert
+ 
+print "X Rotation: " , get_x_rotation(beschleunigung_xout_skaliert, beschleunigung_yout_skaliert, beschleunigung_zout_skaliert)
+print "Y Rotation: " , get_y_rotation(beschleunigung_xout_skaliert, beschleunigung_yout_skaliert, beschleunigung_zout_skaliert)
