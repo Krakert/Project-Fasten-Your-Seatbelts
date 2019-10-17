16b5d9e397de07b93b17d8792a32f28414c99005
Raouls RPI <ursumr@hva.nl>
2019-10-17 20:24:41 +0100

    Initial release of LED sequence

---
+++ WS2812.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+# NeoPixel library strandtest example
+# Author: Tony DiCola (tony@tonydicola.com)
+#
+# Direct port of the Arduino NeoPixel library strandtest example.  Showcases
+# various animations on a strip of NeoPixels.
+
+import time
+from rpi_ws281x import PixelStrip, Color
+
+# Define functions which animate LEDs in various ways.
+def colorWipe(strip, color, wait_ms=1):
+    """Wipe color across display a pixel at a time."""
+    for i in range(strip.numPixels()):
+        strip.setPixelColor(i, color)
+        strip.show()
+        time.sleep(wait_ms / 1000.0)
+
+def setPixelColor(strip, pixelNum, color):
+        """Set color for a pixel"""
+        strip.setPixelColor(pixelNum, color)
+        strip.show()
+
+
+def wheel(pos):
+    """Generate rainbow colors across 0-255 positions."""
+    if pos < 85:
+        return Color(pos * 3, 255 - pos * 3, 0)
+    elif pos < 170:
+        pos -= 85
+        return Color(255 - pos * 3, 0, pos * 3)
+    else:
+        pos -= 170
+        return Color(0, pos * 3, 255 - pos * 3)
+
+
+def rainbow(strip, wait_ms=20, iterations=1):
+    """Draw rainbow that fades across all pixels at once."""
+    for j in range(256 * iterations):
+        for i in range(strip.numPixels()):
+            strip.setPixelColor(i, wheel((i + j) & 255))
+        strip.show()
+        time.sleep(wait_ms / 1000.0)
--- main.py
+++ main.py
@@ -1,21 +1,87 @@
-import servo
+#import servo
 import time
+from rpi_ws281x import PixelStrip, Color
+import WS2812
+import RPi.GPIO as GPIO
+import random
 
-SLEEPTIME = 3
+NUMBER_OF_BOARD_PANELS = 6
+SEQUENCE_LED_ON_TIME = 1 #seconds
+SEQUENCE_LED_OFF_TIME = 0.3 #seconds
 
-servo.setServo(200)
-time.sleep(SLEEPTIME)
+BOARD_PANEL_PIN_1 = 4
+BOARD_PANEL_PIN_2 = 0
+BOARD_PANEL_PIN_3 = 0
+BOARD_PANEL_PIN_4 = 0
+BOARD_PANEL_PIN_5 = 0
+BOARD_PANEL_PIN_BULL = 0
 
-servo.setServo(100)
-time.sleep(SLEEPTIME)
+sequence = []
+sequenseSize = 0
 
-servo.setServo(150)
-time.sleep(SLEEPTIME)
+# LED strip configuration:
+LED_PIN = 18          # GPIO pin connected to the pixels (18 uses PWM!).
+# LED_PIN = 10        # GPIO pin connected to the pixels (10 uses SPI /dev/spidev0.0).
+LED_FREQ_HZ = 800000  # LED signal frequency in hertz (usually 800khz)
+LED_DMA = 10          # DMA channel to use for generating signal (try 10)
+LED_BRIGHTNESS = 50  # Set to 0 for darkest and 255 for brightest
+LED_INVERT = False    # True to invert the signal (when using NPN transistor level shift)
+LED_CHANNEL = 0       # set to '1' for GPIOs 13, 19, 41, 45 or 53
 
-servo.setServo(50)
-time.sleep(SLEEPTIME)
+#color code hexdecimal
+COLORS = [  0x000000,   #black
+                0x200000,   # red
+				0x201000,   # orange
+				#0x202000,   # yellow
+				0x002000,   # green
+				#0x002020,   # lightblue
+				0x000020,   # blue
+				0x100010,   # purple
+				#0x200010,   # pink
+                0x202020, ] #white
 
-servo.setServo(0)
-time.sleep(SLEEPTIME)
+GPIO.setmode(GPIO.BCM)
 
-print ('Done executing program!')
+GPIO.setup(BOARD_PANEL_PIN_1, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.add_event_detect(BOARD_PANEL_PIN_1, GPIO.RISING, bouncetime=500)
+def panelCallback(self):
+    print ('pushed!!!')
+    panelOneHit()
+
+GPIO.add_event_callback(BOARD_PANEL_PIN_1, panelCallback)
+
+def panelOneHit():
+    randomColor = random.randrange(0, 7)
+    WS2812.setPixelColor(strip, 0, COLORS[randomColor])
+
+def generateSequence():
+    randomPanel = random.randrange(1, NUMBER_OF_BOARD_PANELS + 1)
+    print('new number: %d' % (randomPanel))
+    sequence.append(randomPanel)
+    
+    return sequence
+
+def showSequence(newSequence):
+    print('show the sequence')
+    for element in newSequence:
+        print(element)
+        WS2812.setPixelColor(strip, element - 1, COLORS[element])
+        time.sleep(SEQUENCE_LED_ON_TIME)
+        WS2812.colorWipe(strip, COLORS[0])
+        time.sleep(SEQUENCE_LED_OFF_TIME)
+
+# Create NeoPixel object with appropriate configuration.
+strip = PixelStrip(NUMBER_OF_BOARD_PANELS, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL)
+# Intialize the library (must be called once before other functions).
+strip.begin()
+
+while True:
+    newSequence = generateSequence()
+
+    showSequence(newSequence)
+
+    time.sleep(0.5)
+    
+    # WS2812.rainbow(strip)
+
+
