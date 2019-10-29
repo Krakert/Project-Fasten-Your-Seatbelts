c8c0a338b751bb699ff2695ad4a6507191c03f30
Milo Broerse <milo.broerse@gmail.com>
2019-10-29 22:50:56 +0100

    Removed acceleration and cleaned up code

--- gyroscope.py
+++ gyroscope.py
@@ -1,6 +1,6 @@
-#!/usr/bin/python
 import smbus
 import math
+import time
  
 # Register
 power_mgmt_1 = 0x6b
@@ -16,55 +16,28 @@ def read_word(reg):
     return value
  
 def read_word_2c(reg):
-    val = read_word(reg)
-    if (val >= 0x8000):
-        return -((65535 - val) + 1)
+    value = read_word(reg)
+    if value >= 0x8000:
+        return -((65535 - value) + 1)
     else:
-        return val
+        return value
+    
+bus = smbus.SMBus(1) 
+# Used Adress via i2c Detect
+address = 0x68
  
-def dist(a,b):
-    return math.sqrt((a*a)+(b*b))
- 
-def get_y_rotation(x,y,z):
-    radians = math.atan2(x, dist(y,z))
-    return -math.degrees(radians)
- 
-def get_x_rotation(x,y,z):
-    radians = math.atan2(y, dist(x,z))
-    return math.degrees(radians)
- 
-bus = smbus.SMBus(1) # bus = smbus.SMBus(0) fuer Revision 1
-address = 0x68       # via i2cdetect
- 
-# Aktivieren, um das Modul ansprechen zu koennen
+# Activate, to approach the model
 bus.write_byte_data(address, power_mgmt_1, 0)
  
-print "Gyroskop"
-print "--------"
- 
-gyroskop_xout = read_word_2c(0x43)
-gyroskop_yout = read_word_2c(0x45)
-gyroskop_zout = read_word_2c(0x47)
- 
-print "gyroskop_xout: ", ("%5d" % gyroskop_xout), " skaliert: ", (gyroskop_xout / 131)
-print "gyroskop_yout: ", ("%5d" % gyroskop_yout), " skaliert: ", (gyroskop_yout / 131)
-print "gyroskop_zout: ", ("%5d" % gyroskop_zout), " skaliert: ", (gyroskop_zout / 131)
- 
-print
-print "Beschleunigungssensor"
-print "---------------------"
- 
-beschleunigung_xout = read_word_2c(0x3b)
-beschleunigung_yout = read_word_2c(0x3d)
-beschleunigung_zout = read_word_2c(0x3f)
- 
-beschleunigung_xout_skaliert = beschleunigung_xout / 16384.0
-beschleunigung_yout_skaliert = beschleunigung_yout / 16384.0
-beschleunigung_zout_skaliert = beschleunigung_zout / 16384.0
- 
-print "beschleunigung_xout: ", ("%6d" % beschleunigung_xout), " skaliert: ", beschleunigung_xout_skaliert
-print "beschleunigung_yout: ", ("%6d" % beschleunigung_yout), " skaliert: ", beschleunigung_yout_skaliert
-print "beschleunigung_zout: ", ("%6d" % beschleunigung_zout), " skaliert: ", beschleunigung_zout_skaliert
- 
-print "X Rotation: " , get_x_rotation(beschleunigung_xout_skaliert, beschleunigung_yout_skaliert, beschleunigung_zout_skaliert)
-print "Y Rotation: " , get_y_rotation(beschleunigung_xout_skaliert, beschleunigung_yout_skaliert, beschleunigung_zout_skaliert)
+print "Gyroscope Data"
+while True:
+    SCALED_TO_USEFUL_DATA = 128.0
+    gyroscope_xout = read_word_2c(0x43)
+    gyroscope_yout = read_word_2c(0x45)
+    gyroscope_zout = read_word_2c(0x47)
+    print gyroscope_xout
+    print "--------"
+    print "GyroscopeX: %5d Scaled Data: %.2f" % (gyroscope_xout, gyroscope_xout / SCALED_TO_USEFUL_DATA)
+    print "GyroscopeY: %5d Scaled Data: %.2f" % (gyroscope_yout, gyroscope_yout / SCALED_TO_USEFUL_DATA)
+    print "GyroscopeZ: %5d Scaled Data: %.2f" % (gyroscope_zout, gyroscope_zout / SCALED_TO_USEFUL_DATA)
+    time.sleep(1)
