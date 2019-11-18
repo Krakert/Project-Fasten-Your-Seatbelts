d903910d35a222612985c886045cf82ae3aac08f
Raouls RPI <ursumr@hva.nl>
2019-11-18 21:45:32 +0000

    Fixed a bug #1 and continued working on servo.py

--- servo.py
+++ servo.py
@@ -1,26 +1,74 @@
 import RPi.GPIO as GPIO
 import time
 
+# this library is used to generate a timer
+# https://www.geeksforgeeks.org/timer-objects-python/
+import threading
+
+SERVO_PIN 16
+SERVO_PWM_FREQ = 50 #frequentie in Hz
+
+INTERVAL_SECONDS = 0.005 #5ms
+
+START_POS_SERVO = 7.5 #can range between 5 and 10
+SERVO_STEP_PER_DEGREE = 0.025
+targetPosition = 0
+currentPosition = 0
+
 GPIO.setwarnings(False)
 
+def timerCallback():
+    global targetPosition
+    print("Test\n")
+    
+    if targetPosition < currentPosition:
+        servoPos += SERVO_STEP_PER_DEGREE
+        pwm.ChangeDutyCycle(servoPos)
+
+    if targetPosition > currentPosition:
+        servoPos -= SERVO_STEP_PER_DEGREE
+        pwm.ChangeDutyCycle(servoPos)
+    
+
+#start the timer
+timer = threading.Timer(INTERVAL_SECONDS, timerCallback)
+
 GPIO.setmode(GPIO.BOARD)
-#gebruik pin 11 voor de servo
-GPIO.setup(11,GPIO.OUT)
-#zet de pwm op pin 11 op 50Hz
-pwm=GPIO.PWM(11,50)
+#use GPIO 16(PWM) for the servo
+GPIO.setup(SERVO_PIN, GPIO.OUT)
+#set PWM at 50Hz
+pwm=GPIO.PWM(SERVO_PIN, SERVO_PWM_FREQ)
+
+
 
 #start PWM
-pwm.start(5)
-time.sleep(0.2)
+def startPWM():
+    global currentPosition
+    global targetPosition
 
-#degrees moeten tussen 0 en 200 zijn
+    pwm.start(START_POS_SERVO)
+    currentPosition = START_POS_SERVO
+    targetPosition = START_POS_SERVO
+    time.sleep(0.5)
+
+#this function can start the timer used for controlling the servo
+def startTimer():
+    timer.start()
+
+#this function can stop the timer used for controlling the servo
+def stopTimer():
+    timer.cancel()
+
+#degrees must be a value between 0 and 200
+#if the value is not valid the function will return False
 def setServo(degrees):
-    servoPos = (degrees / 40) + 5
-    pwm.ChangeDutyCycle(servoPos)
+    global targetPosition
 
-# pwm.ChangeDutyCycle(7.5)
-# time.sleep(2)
+    if degrees <= 200 and degrees >= 0:
+        targetPosition = (degrees / 40) + 5
+        return True
+    
+    else:
+        print("Used a invalid value for setServo!\n")
+        return False
 
-# pwm.ChangeDutyCycle(10)
-# time.sleep(2)
-# print ('Done executing program!')
