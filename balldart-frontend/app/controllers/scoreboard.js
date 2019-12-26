dbd7be53a5356bac721b88aaf6e60843c939eef4
Milo Broerse <milo.broerse@gmail.com>
2019-12-26 22:15:37 +0100

    Sort the Scorebord

--- balldart-frontend/app/controllers/scoreboard.js
+++ balldart-frontend/app/controllers/scoreboard.js
@@ -1,11 +1,37 @@
 import Controller from '@ember/controller';
-import { sort } from '@ember/object/computed'
-import { set } from '@ember/object';
+import { sort } from '@ember/object/computed';
+import { set, computed } from '@ember/object';
 
 export default Controller.extend({
   queryParams: ['data'],
-  todosSorting: Object.freeze(['data']),
-  arrangedContent: sort('model', 'todosSorting'),
+  arrangedContent: sort('enrichedModel', 'sortingOrder'),
+
+  sortingOrder: computed('data', function() {
+    let data = this.data;
+    if(data === "totalPoints"){
+      return ["totalPoints:desc"];
+    } else if(data === "highestPoints"){
+      return ["highestPoints:desc"];
+    } else{
+      return ["averagePoints:desc"];
+    }
+  }),
+  enrichedModel: computed('model', function() {
+    let array = [];
+    let model = this.model;
+    if(model){
+      model.forEach((m) => {
+        let average = 0;
+        if(m.numberOfRounds){
+          average = Number(m.totalPoints) / Number(m.numberOfRounds);
+        }
+        m.set('averagePoints', average);
+        array.push(m);
+      })
+      console.log(array);
+    }
+    return array;
+  }),
 
   actions: {
     sort: function(data) {
